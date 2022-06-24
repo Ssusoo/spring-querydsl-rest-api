4528288b9ee09a94f51dc03c80a7ad310130cde0
zzanggoon8 <zzanggoon8@gmail.com>
2022-06-24 23:50:04 +0900

    etc

--- src/test/java/me/ssu/springquerydslrestapi/domains/event/domain/EventTest.java
+++ src/test/java/me/ssu/springquerydslrestapi/domains/event/domain/EventTest.java
@@ -5,21 +5,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventTest {
-
-	@Test
-	void javaBean() {
-		// Given
-		String eventName = "이벤트 이름";
-		String eventDescription = "이벤트 설명";
-
-		// When
-		Event event = new Event();
-		var savedEvent = event.builder()
-				.eventName(eventName)
-				.eventDescription(eventDescription)
-				.build();
-
-
-		// Then
-	}
 }
